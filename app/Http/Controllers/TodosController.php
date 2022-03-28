74930e8f5179a91bfde7aa7fe5e205aae0c5c1fb
Adeleke Hammed Lekan <lekhad19@gmail.com>
2022-03-28 18:41:41 -0400

    Styling views with Bootstrap in Laravel

--- app/Http/Controllers/TodosController.php
+++ app/Http/Controllers/TodosController.php
@@ -11,8 +11,8 @@ class TodosController extends Controller
         // fetch all todos from the database
 
         // display them in the todos.index page
-        // $todos = Todo::all();
+        $todos = Todo::all();
         // return view('todos.index')->with('todos', $todos);
-        return view('todos.index')->with('todos', Todo::all());
+        return view('todos.index')->with('todos', $todos);
     }
 }
--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -4,10 +4,34 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
     <title>Document</title>
 </head>
 <body>
-    <h1>TODOs PAGE</h1>
-    {{ $todos }}
+   <div class="container">
+       <h1 class="text-center my-5">TODOS PAGE</h1>
+       <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card card-default">
+                    <div class="card-header">
+                        ToDos
+                    </div>
+                </div>
+        
+                <div class="card-body">
+                    <div class="list-group">
+                        @foreach ($todos as $todo)
+                            <li class="list-group-item">
+                                {{ $todo->name }}
+
+                                <button class="btn btn-primary btn-sm float-right">View</button>
+                            </li>
+                        @endforeach
+                    </div>
+                </div>
+            </div>
+       </div>
+   </div>
 </body>
 </html>
