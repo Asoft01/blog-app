f296ea5d3eeb92a08d0cae56497b3caabd860b41
Adeleke Hammed Lekan <lekhad19@gmail.com>
2022-06-03 05:40:25 -0400

    Introducing route model binding in Laravel

--- app/Http/Controllers/TodosController.php
+++ app/Http/Controllers/TodosController.php
@@ -16,9 +16,13 @@ class TodosController extends Controller
         return view('todos.index')->with('todos', $todos);
     }
 
-    public function show($todoId){
-        // $todo = Todo::find($todoId);
-        return view('todos.show')->with('todo', Todo::find($todoId));
+    // public function show($todoId){
+    //     // $todo = Todo::find($todoId);
+    //     return view('todos.show')->with('todo', Todo::find($todoId));
+    // }
+
+    public function show(Todo $todo){
+        return view('todos.show')->with('todo', $todo);
     }
 
     public function create(){
@@ -45,13 +49,42 @@ class TodosController extends Controller
         return redirect('/todos');
     }
 
-    public function edit($todoId){
-        $todo = Todo::find($todoId);
+    // public function edit($todoId){
+    //     $todo = Todo::find($todoId);
+    //     return view('todos.edit')->with('todo', $todo);
+    // }
+
+    public function edit(Todo $todo){
         return view('todos.edit')->with('todo', $todo);
     }
 
-    public function update(Request $request, $todoId){
-        // $this->validate($request(), )
+    // public function update(Request $request, $todoId){
+    //     // $this->validate($request(), )
+    //     $data = $request->all();
+        
+    //     // dd($data); die;
+    //     $rules = [
+    //         'name'=> 'required|min:6|max:12',
+    //         'description' => 'required',
+    //     ];
+        
+    //     $customMessages = [
+    //         'name.required' =>  'Name is required', 
+    //         'description.required'=> 'Description is required',
+    //     ];
+
+    //     $this->validate($request, $rules, $customMessages);
+
+    //     $todo = Todo::find($todoId); 
+    //     $todo->name = $data['name'];
+
+    //     $todo->description = $data['description'];
+
+    //     $todo->save();
+    //     return redirect('/todos');
+    // }
+
+    public function update(Request $request, Todo $todo){
         $data = $request->all();
         
         // dd($data); die;
@@ -66,18 +99,19 @@ class TodosController extends Controller
         ];
 
         $this->validate($request, $rules, $customMessages);
-
-        $todo = Todo::find($todoId); 
         $todo->name = $data['name'];
-
         $todo->description = $data['description'];
-
         $todo->save();
         return redirect('/todos');
     }
 
-    public function delete($todoId){
-        $todo = Todo::find($todoId);
+    // public function delete($todoId){
+    //     $todo = Todo::find($todoId);
+    //     $todo->delete();
+    //     return redirect('/todos');
+    // }
+
+    public function delete(Todo $todo){
         $todo->delete();
         return redirect('/todos');
     }
